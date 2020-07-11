ae570004bcce4874b828a6c56b9732c147af05c4
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-11 08:44:23 -0400

    add formsarray

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
-  styleUrls: ['./reactive-forms.component.scss']
+  styleUrls: ['./reactive-forms.component.scss'],
 })
-export class ReactiveFormsComponent  {
-  profileForm = new FormGroup({
-    first: new FormControl('', Validators.required),
-    last: new FormControl(''),
-  });
+export class ReactiveFormsComponent {
+  profileForm: FormGroup;
+  addresses: FormArray;
 
-  subscribed = new FormControl('');
+  get itemControls() {
+    return (this.profileForm.get('addresses') as FormArray).controls;
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.profileForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl(''),
+      addresses: new FormArray([]),
+      subscribed: new FormControl(),
+      disabledInput: new FormControl({value: '', disabled: true})
+    });
+
+    this.addItem();
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      city: ''
+    });
+  }
+
+  addItem(): void {
+    this.addresses = this.profileForm.get('addresses') as FormArray;
+    this.addresses.push(this.createItem());
+  }
 }
